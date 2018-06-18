3fe2560ff2e14847c3168dbf228945b7de45c953
JanPel <jan.pel@futurefacts.nl>
2018-06-18 16:21:50 +0200

    added script to read a CBSfile into the database

---
+++ into_database.py
@@ -0,0 +1,26 @@
+import cbsodata
+import pandas as pd
+import sqlalchemy
+from credentials import login, database
+
+raw_data_2015 = cbsodata.get_data('83220NED')
+
+wijk_data = [rij for rij in raw_data_2015 if 'Wijk' in rij['SoortRegio_2']]
+for wijk in wijk_data:
+    wijk['Gemeentenaam_1'] = wijk['Gemeentenaam_1'].strip()
+
+result2015 = pd.DataFrame.from_dict(wijk_data, orient='columns' , dtype=None)
+
+HOST = database['host']
+DB = database['DB']
+USER = login['user']
+PASSWORD = login['password']
+
+connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                           format(USER, PASSWORD,
+                                                  HOST, DB))
+
+result2015.to_sql(con= connection, name='wijken', if_exists='replace')
+
+
+result2015
