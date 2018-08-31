7e41b4300cd7c035e167e93e239c59c57ddacda4
JanPel <jan.pel@futurefacts.nl>
2018-08-31 16:39:03 +0200

    wrote reading of CBS data into functions

--- functional_CBS.py
+++ functional_CBS.py
@@ -2,11 +2,12 @@ import cbsodata
 import pandas as pd
 import sqlalchemy
 from credentials import login,database
+from table_functions import process_data_buurten
 cijfers_buurten_en_wijken = {'BuurtenenWijken2015':'83220NED',
                              'BuurtenenWijken2016':'83487NED',
                              'BuurtenenWijken2017':'83765NED',
                              'BuurtenenWijken2018':'84286NED'}
-## HERE WE CONNECT TO THE DATABASE AND THEN WRITE THE DATA INTO THE DATABASE.
+## HERE THE CREDENTIALS ARE ASSIGNED 
 HOST = database['host']
 DB = database['DB']
 USER = login['user']
@@ -14,33 +15,10 @@ PASSWORD = login['password']
 
 #this function translates the column names, it takes away the numbering, i.e.
 # codering_3 becomes codering
-def translator(string):
-    exceptions = ['Appartement','VrijstaandeWoning','TweeOnderEenKapWoning',
-                 'Hoekwoning','Tussenwoning','Huurwoning','EigenWoning']
-    for x in exceptions:
-        if x in string:
-            return string
-    split = string.split('_')
-    if len(string.split('_')) == 1:
-        return split[0]
-    else:
-        return '_'.join(split[:-1])
 
 #This function is used to download the data from CBS and make it into a desired
 #dataframe.
-def process_data(CBS_codering):
-    data = cbsodata.get_data(CBS_codering)
-    collecting = {}
-    for row in reversed(data):
-        collecting[int('1'+ row['Codering_3'][2:])] = row
-        row['Perioden'] = 2015
-        row['Gemeentenaam_1'] = row['Gemeentenaam_1'].strip()
-        row['SoortRegio_2'] = row['SoortRegio_2'].strip()
-    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
-    df.rename(columns = translator,inplace = True) 
-    return df
-
-
+## HERE WE CONNECT TO THE DATABASE, USING SQL ALCHEMY.
 engine = sqlalchemy.create_engine(
         'mysql://{0}:{1}@{2}/{3}'.format(USER,PASSWORD,HOST,DB))
 dataset = {}
@@ -48,7 +26,8 @@ dataset = {}
 #datasets.
 for x,y in cijfers_buurten_en_wijken.items():
     print('downloading data buurten en wijken from {}'.format(x))
-    data = process_data(y)
+    data = process_data_buurten(y)
+    print(data.iloc[1])
     dataset[x] = data
     conn = engine.connect()
     data.to_sql(con= conn, name=x, if_exists='replace')    
---
+++ table_functions.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import cbsodata
+
+def translator(string):
+    exceptions = ['Appartement','VrijstaandeWoning','TweeOnderEenKapWoning',
+                 'Hoekwoning','Tussenwoning','Huurwoning','EigenWoning']
+    for x in exceptions:
+        if x in string:
+            return string
+    split = string.split('_')
+    if len(string.split('_')) == 1:
+        return split[0]
+    else:
+        return '_'.join(split[:-1])
+
+def process_data_buurten(CBS_codering):
+    data = cbsodata.get_data(CBS_codering)
+    collecting = {}
+    for row in reversed(data):
+        collecting[int('1'+ row['Codering_3'][2:])] = row
+        row['Perioden'] = 2015
+        row['Gemeentenaam_1'] = row['Gemeentenaam_1'].strip()
+        row['SoortRegio_2'] = row['SoortRegio_2'].strip()
+    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
+    df.rename(columns = translator,inplace = True) 
+    return df
+
+def process_data_jongeren(CBS_codering):
+    data = cbsodata.get_data(CBS_codering)
+    collecting = {}
+    for row in data:
+        if len(row['Perioden']) != 4:
+            continue
+        identifier = int('1'+row['Codering_3'][2:])
+        if 'VormenVanJeugdzorg' in row.keys():
+            TypeJeugdzorg = row['VormenVanJeugdzorg']
+        else:
+            TypeJeugdzorg = row['TypeJeugdzorg']
+            
+        if identifier in collecting:
+            result = collecting[identifier]
+            result[TypeJeugdzorg] = row['TotaalJongerenMetJeugdzorg_5']
+        else:
+            row[TypeJeugdzorg] = row['TotaalJongerenMetJeugdzorg_5']
+            row['Gemeentenaam_1'] = row['Gemeentenaam_1'].strip()
+            row['Wijken'] = row['Wijken'].strip()
+            collecting[identifier] = row            
+    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
+    df.rename(columns = translator,inplace = True)
+    return df    
+
+def process_data_wmo(CBS_codering):
+    data = cbsodata.get_data(CBS_codering)
+    collecting = {}
+    for row in reversed(data):
+        if len(row['Perioden']) != 4 or len(row['Codering_3'].rstrip()) == 0:
+            continue
+        identifier = int('1' + row['Codering_3'][2:])
+        Financieringsvorm = row['Financieringsvorm']
+        if identifier in collecting:
+            result = collecting[identifier]
+            result[Financieringsvorm] = row['WmoClienten_5']
+            result[Financieringsvorm + ' per100Inwoners_6'] = row['WmoClientenPer1000Inwoners_6']
+        else:
+            row[Financieringsvorm] = row['WmoClienten_5']
+            row[Financieringsvorm + ' per100Inwoners_6'] = row['WmoClientenPer1000Inwoners_6']
+            collecting[identifier] = row
+    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
+    df.rename(columns = translator,inplace = True)
+    return df
+
+def process_data_sociale_voorzieningen(CBS_codering):
+    data = cbsodata.get_data(CBS_codering)
+    collecting = {}
+    for row in data:
+        if len(row['Perioden']) != 4 or len(row['Codering_3'].rstrip()) == 0:
+                continue
+        identifier = int('1' + row['Codering_3'][2:])
+        collecting[identifier] = row
+    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
+    df.rename(columns = translator,inplace = True)
+    return df
+
+def process_data_gezondheidsmonitor(CBS_codering):
+    data = cbsodata.get_data(CBS_codering)
+    buurten2016 = cbsodata.get_data('83487NED')
+    translate2016 = {}
+    for row in buurten2016:
+        if row['SoortRegio_2'].rstrip() == 'Gemeente':
+            translate2016[row['WijkenEnBuurten']] = row['Codering_3']
+            
+    collecting = {}
+    for row in data:
+        try:
+            identifier = int('1' + translate2016[row['RegioS']][2:])
+            collecting[identifier] = row
+        except:
+            pass
+    df = pd.DataFrame.from_dict(collecting, orient='index', dtype=None)
+    df.rename(columns = translator,inplace = True)
+    return df
