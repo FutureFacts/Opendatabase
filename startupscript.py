790abd31cc0b708531551d2117254349c59390f7
JanPel <jan.pel@futurefacts.nl>
2018-09-21 14:08:30 +0200

    added option for combining different neighborhoods

---
+++ startupscript.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import cbsodata
+import sqlalchemy
+from credentials import login, database
+from process_functions.table_functions import (translator,
+                                               process_data_buurten)
+from databronnen import cijfers_buurten_en_wijken
+
+BuurtenenWijken = {key:item for key,item in cijfers_buurten_en_wijken.items()
+                   if 'BuurtenenWijken' in key}
+print(BuurtenenWijken)                   
+
+## HERE WE CONNECT TO THE DATABASE
+HOST = database['host']
+DB = database['DB']
+USER = login['user']
+PASSWORD = login['password']
+engine = sqlalchemy.create_engine(
+        'mysql://{0}:{1}@{2}/{3}'.format(USER,PASSWORD,HOST,DB))
+
+conn = engine.connect()
+for name,item in BuurtenenWijken.items():
+    source = item[0]
+    function = item[1]
+    data = function(source,name)
+    vertaaltabel = data[['Gemeentenaam','WijkenEnBuurten','SoortRegio','Codering']]
+    vertaaltabel['MMR_indeling'] = vertaaltabel['Codering']
+    vertaaltabel['soort_regio_nieuwe_indeling'] = vertaaltabel['SoortRegio']
+    vertaaltabel[(vertaaltabel['Gemeentenaam'] == 'Terneuzen') & (vertaaltabel['SoortRegio'] == 'Wijk')]
+    table_name = 'VERTAAL_' + name[-4:] 
+    vertaaltabel.to_sql(con= conn, name=table_name,
+                        if_exists='replace')
---
+++ update_MMR_wijken.py
@@ -0,0 +1,40 @@
+import os
+import sqlalchemy
+import pandas as pd
+from credentials import login, database
+
+
+HOST = database['host']
+DB = database['DB']
+USER = login['user']
+PASSWORD = login['password']
+engine = sqlalchemy.create_engine(
+        'mysql://{0}:{1}@{2}/{3}'.format(USER,PASSWORD,HOST,DB))
+
+conn = engine.connect()
+
+walker = os.walk('./Vertaling_wijken/')
+next(walker)
+years = {}
+for directory,_, files in walker:
+    print(directory,files)
+    year = [directory + '/' + x for x in files if x[-4:] == 'xlsx']
+    print(year)
+    years[directory[-4:]] = year
+
+def update_dataframe(df,data):
+    for index, row in data.iterrows():
+        select = df['Codering'].map(lambda x: x.rstrip()) == row['Wijkcode']
+        gemeente = ' (' + df.loc[select].iloc[0]['Gemeentenaam'] + ')'
+        df.loc[select,['MMR_indeling','soort_regio_nieuwe_indeling']] = (row['MMR-wijk'] + gemeente ,row['SoortRegio'] )    
+    return df
+
+for year in years.keys():
+    table_name =  'VERTAAL_' + year
+    SQL_statement = "SELECT * FROM " + database['DB'] + '.' + table_name
+    df = pd.read_sql(SQL_statement, conn)
+    for gemeente in years[year]:
+        data = pd.read_excel(gemeente)
+        df = update_dataframe(df,data)
+    df.to_sql(con = conn, name = table_name,
+              if_exists = 'replace')
